15dda1621ec81a21a9fe6ab039ff8697caf79805
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2022-01-04 07:35:52 -0300

    feat: cadastro de entregas

--- src/routes.ts
+++ src/routes.ts
@@ -2,19 +2,23 @@ import { Router } from 'express'
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController'
 import { AuthenticateClientController } from './modules/account/authenticateUser/AuthenticateClientController'
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController'
+import { CreateDeliveryController } from './modules/deliveries/createDelivery/CreateDeliveryController'
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController'
 
-const routes = Router()
+const routes = Router() 
 
 const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 const createDeliverymanController = new CreateDeliverymanController()
+const createDeliveryController = new CreateDeliveryController()
 
 routes.post('/client/', createClientController.handle)
 routes.post('/client/authenticate/', authenticateClientController.handle)
 
 routes.post('/deliveryman/', createDeliverymanController.handle)
 routes.post('/deliveryman/authenticate/', authenticateDeliverymanController.handle)
+
+routes.post('/delivery/', createDeliveryController.handle)
 
 export { routes }
